4c1bd9ff9b6321c5039dd79695108377a4bea7cb
¨zzt¨ <zztmercury@gmail.com>
2015-02-28 18:48:26 +0800

    add class com.sostan.whisper.net.Register

--- whisper/src/com/sostan/whisper/Config.java
+++ whisper/src/com/sostan/whisper/Config.java
@@ -9,9 +9,21 @@ import android.content.SharedPreferences.Editor;
 public class Config {
 
     public static final String APP_ID = "com.sostan.whisper";
-    public static final String KEY_TOKEN = "token";
     public static final String CHARSET = "utf-8";
     public static final String SERVER_URL = "";
+
+    public static final String ACTION_LOGIN = "login";
+    public static final String ACTION_REGISTER = "register";
+
+    public static final String KEY_TOKEN = "token";
+    public static final String KEY_ACTION = "action";
+    public static final String KEY_PHONE_MD5 = "phone_md5";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_STATUS = "status";
+
+    public static final int RESULT_STATUS_SUCCESS = 1;
+    public static final int RESULT_STATUS_FAIL = 0;
+    public static final int RESULT_STATUS_INVALID_TOKEN = 2;
 
     public static String getCachedToken(Context context) {
         return context.getSharedPreferences(APP_ID,Context.MODE_PRIVATE).getString(KEY_TOKEN,null);
