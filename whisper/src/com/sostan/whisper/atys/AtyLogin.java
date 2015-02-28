e61d23846201df21ff9dcda417a4704e4b22d8f4
¨zzt¨ <zztmercury@gmail.com>
2015-02-28 20:31:24 +0800

    add class com.sostan.whisper.net.Register

--- whisper/src/com/sostan/whisper/atys/AtyLogin.java
+++ whisper/src/com/sostan/whisper/atys/AtyLogin.java
@@ -2,15 +2,41 @@ package com.sostan.whisper.atys;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 import com.sostan.whisper.R;
+import com.sostan.whisper.net.Login;
 
 /**
  * Created by 35517_000 on 2015/2/27.
  */
 public class AtyLogin extends Activity {
+    private EditText etPhone = null;
+    private EditText etPassword = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aty_login);
+
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+
+        findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(etPhone.getText())) {
+                    Toast.makeText(AtyLogin.this,getString(R.string.enter_phone_num),Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else if (TextUtils.isEmpty(etPassword.getText())) {
+                    Toast.makeText(AtyLogin.this,getString(R.string.enter_password),Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+            }
+        });
     }
 }
---
+++ whisper/src/com/sostan/whisper/tools/MD5Tool.java
@@ -0,0 +1,7 @@
+package com.sostan.whisper.tools;
+
+/**
+ * Created by zzt on 15-2-28.
+ */
+public class MD5Tool {
+}
